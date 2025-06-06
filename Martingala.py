149128936392d04514362b1bde7fb8706a226e54
goten009 <goten0099@gmail.com>
2025-06-05 23:51:06 -0500

    Update Martingala.py

--- Martingala.py
+++ Martingala.py
@@ -11,8 +11,9 @@ st.title("📈 Simulador de Apuesta con\nMartingala Reducida")
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
 # ✅ Usamos json.loads porque el secreto es una cadena JSON válida
-credenciales_dict = st.secrets["GOOGLE_CREDENTIALS"]
+credenciales_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
 credenciales = ServiceAccountCredentials.from_json_keyfile_dict(credenciales_dict, scope)
+
 
 cliente = gspread.authorize(credenciales)
 spreadsheet = cliente.open("Control Apuestas Rentables")
